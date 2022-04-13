2aed38806fa1bdae2cc34ab64b680a97b8bd15e5
LenaKotik <alehzeygom@gmail.com>
2022-04-13 21:00:24 +0300

    database connection implemented (with MySql supported)

--- DataProvider.cs
+++ DataProvider.cs
@@ -4,7 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+#if USE_MYSQL
+using MySql.Data.MySqlClient;
+#else
 using System.Data.SqlClient;
+#endif
 #nullable enable
 
 namespace Sales
@@ -15,8 +20,7 @@ namespace Sales
     /// </summary>
     static class DataProvider
     {
-        static readonly string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False";
-
+        static readonly string connStr = @"workstation id=qqqsales.mssql.somee.com;packet size=4096;user id=swagmav_SQLLogin_1;pwd=m1vbu3gz7c;data source=qqqsales.mssql.somee.com;persist security info=False;initial catalog=qqqsales";
         #region file system stuff
         static List<string> GetDirectories(string path)
         {
@@ -36,20 +40,48 @@ namespace Sales
         /// <returns>null if such product does not exist, otherwise - the existing product</returns>
         public static Product? SearchProduct(string Device, string Vendor, string Model, string Type)
             => new Product(Device, Vendor, Model, Type, @"D:\ProjectRoot\CSharpProj\Sales\Models\"+Device+'\\'+Vendor+'\\'+Model+'\\'+Type + ".eps");
-        #endregion
-        #region database stuff
+#endregion
+#region database stuff
         public static void AddUser(User u)
         {
+#if USE_MYSQL
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"INSERT INTO Users VALUES('{u.Name}', '{u.Code}', '{u.Password}', {(u.IsAdmin ? 1 : 0)}); ", conn);
+                cmd.ExecuteNonQuery();
+            }
+#else
             using (SqlConnection conn = new SqlConnection(connStr))
             {
                 conn.Open();
                 SqlCommand cmd = new SqlCommand($"INSERT INTO Users VALUES('{u.Name}', '{u.Code}', '{u.Password}', {(u.IsAdmin?1:0)}); ", conn);
                 cmd.ExecuteNonQuery();
             }
+#endif
         }
         /// <returns>null if such user does not exist, otherwise - the user found</returns>
         public static User? VerifyUser(string code, string password)
         {
+#if USE_MYSQL
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"SELECT * FROM Users WHERE code = '{code}' AND password = '{password}'; ", conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                // Deserialize into list
+                List<User> u = new List<User>();
+                while (rdr.Read())
+                    u.Add(new User()
+                    {
+                        Name = (string)rdr["username"],
+                        Code = (string)rdr["code"],
+                        Password = (string)rdr["password"],
+                        IsAdmin = (bool)rdr["is_admin"],
+                    });
+                return u.SingleOrDefault();
+            }
+#else
             using (SqlConnection conn = new SqlConnection(connStr))
             {
                 conn.Open();
@@ -67,8 +99,9 @@ namespace Sales
                     });
                 return u.SingleOrDefault();
             }
+#endif
         }
-        #endregion
+#endregion
     }
 }
 #nullable restore
